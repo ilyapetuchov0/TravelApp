b99dfaeea1f36e1ed90a2ee483a786d7fa6908e7
ilya <ilyapetuchov0@gmail.com>
2023-10-14 17:05:08 +0300

    Added Camera and scanning QR-code

--- app/src/main/java/com/example/travelapp/MainActivity.java
+++ app/src/main/java/com/example/travelapp/MainActivity.java
@@ -1,14 +1,83 @@
 package com.example.travelapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.travelapp.databinding.ActivityMainBinding;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+       if (isGranted) {
+            showCamera();
+       }
+       else {
+           // Show why user need this permission
+       }
+    });
+
+    private ActivityResultLauncher<ScanOptions> qrCodeLauncher = registerForActivityResult(new ScanContract(), result -> {
+       if (result.getContents() == null) {
+           Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+       } else {
+           setResult(result.getContents());
+       }
+    });
+
+    private void setResult(String contents) {
+        binding.textResult.setText(contents);
+    }
+
+
+    private void showCamera() {
+        ScanOptions options = new ScanOptions();
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE);
+        options.setPrompt("Отсканируйте QR-код");
+        options.setCameraId(0);
+        options.setBeepEnabled(false);
+        options.setBarcodeImageEnabled(true);
+        options.setOrientationLocked(false);
+
+        qrCodeLauncher.launch(options);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        initBinding();
+        initViews();
+    }
+
+    private void initViews() {
+        binding.fab.setOnClickListener(v -> {
+            checkPermissionAndShowActivity(this);
+        });
+    }
+
+    private void checkPermissionAndShowActivity(Context context) {
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            showCamera();
+        } else if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+            Toast.makeText(context, "Camera permission required", Toast.LENGTH_LONG).show();
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+        }
+    }
+
+    private void initBinding() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
